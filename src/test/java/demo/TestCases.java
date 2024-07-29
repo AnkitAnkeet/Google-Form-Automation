b8dd4dc6bdf5b8804a778c4f886fdf138c292f03
GOD <ankitsahoo740@gmail.com>
2024-07-29 19:24:42 +0530

    ME_QA_XGOOGLE_FORM_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -14,13 +14,18 @@ import java.util.logging.Level;
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
+
 public class TestCases {
     ChromeDriver driver;
+   
+   
 
     /*
      * TODO: Write your tests here with testng @Test annotation. 
      * Follow `testCase01` `testCase02`... format or what is provided in instructions
      */
+    
+   
 
      
     /*
@@ -47,6 +52,7 @@ public class TestCases {
         driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
+        //wrappers = new Wrappers(driver);
     }
 
     @AfterTest
@@ -56,4 +62,55 @@ public class TestCases {
         driver.quit();
 
     }
+    @Test
+    public void testCase01() throws InterruptedException {
+        System.out.println("Start Test case: Google Form");
+        //get to google form
+        driver.get("https://forms.gle/wjPkzeSEk1CM7KgGA");
+        Thread.sleep(9000);
+        try{
+           
+            //fill the name textbox
+            Wrappers.writeText(driver, By.xpath("//span[text()='Name']/../../..//following-sibling::div//input"),"Crio Learner");
+            //fill the reason in textbox with ""I want to be the best QA Engineer! 1710572021(variable)"
+            Wrappers.writeText(driver, By.xpath("//textarea[@class='KHxj8b tL9Q4c']"), "I want to be the best QA Engineer! " + Wrappers.calculateEpochTimeToString(0));
+            //rate the testing experience via the radio button
+            Wrappers.clickOnCheckBox(driver, By.xpath("//div[@class='SG0AAe']//span[contains(@class,'OIC90c')]"),"3 - 5");
+            //select java
+            Wrappers.clickOnCheckBox(driver, By.xpath("//span[@class='aDTYNe snByac n5vBHf OIC90c']"),"Java");
+            //select Selenium
+            Wrappers.clickOnCheckBox(driver, By.xpath("//span[@class='aDTYNe snByac n5vBHf OIC90c']"),"Selenium");
+            //select TestNG
+            Wrappers.clickOnCheckBox(driver, By.xpath("//span[@class='aDTYNe snByac n5vBHf OIC90c']"),"TestNG");
+            //click on the dropdown
+            Wrappers.click(driver, By.xpath("//div[@class='e2CuFe eU809d']"));
+            //select from dropdown
+            Wrappers.selectFromDropdown(driver, By.xpath("//div[contains(@class,'ncFHed')]/div//span[normalize-space(text())]"),"Mr");
+            //give the seven date before current date in the field
+            Wrappers.writeText(driver, By.xpath("//input[@type='date']"), Wrappers.calculateDateTimeToString("dd/MM/yyyy", (long) 6.048e+8 ));
+
+            //provide the current time (hour)
+            Wrappers.writeText(driver, By.xpath("//input[@aria-label='Hour']"), Wrappers.calculateDateTimeToString("HH",0));
+             //provide the current time (minute)
+             Wrappers.writeText(driver, By.xpath("//input[@aria-label='Minute']"), Wrappers.calculateDateTimeToString("mm",0));
+             //click on submit button
+             Wrappers.click(driver, By.xpath("//span[text()='Submit']"));
+             
+             //verify page 
+             Thread.sleep(5000);
+             System.out.println(Wrappers.returnText(driver, By.xpath("//div[@class='vHW8K']")));
+
+
+            
+
+        }catch (Exception e){
+                e.printStackTrace();
+                System.out.println("Failed");
+        }
+        System.out.println("End Test case = Google form");
+        
+    }
+
+
+
 }
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -2,7 +2,9 @@ package demo.wrappers;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.Duration;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
@@ -10,10 +12,99 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import java.time.Duration;
+
 
 public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+    //private ChromeDriver driver;
+
+    // Constructor that accepts a ChromeDriver object
+    // public Wrappers(ChromeDriver driver) {
+    //     this.driver = driver;
+    
+    public static void writeText(ChromeDriver driver, By selector, String textToSend) throws Exception {
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(8));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(selector));
+        WebElement element = driver.findElement(selector);
+        element.clear();
+        element.sendKeys(textToSend);
+        Thread.sleep(3000);
+    }
+
+
+    public static String calculateDateTimeToString(String formatString, long offsetInMs){
+        LocalDateTime now = LocalDateTime.now();
+       long seconds = offsetInMs/1000;
+        long nanos = (offsetInMs%1000)*1000000;
+        LocalDateTime newDateTime = now.minus(Duration.ofSeconds(seconds, nanos));
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(formatString);
+        String formattedDateTime = newDateTime.format(formatter);
+
+        return formattedDateTime;
+
+    }
+
+
+    public static String calculateEpochTimeToString(int offsetInMs){
+        Instant now = Instant.now();
+        Instant newInstant = now.plusMillis(offsetInMs);
+        long epochMilli = newInstant.toEpochMilli();
+        return String.valueOf(epochMilli);
+    }
+
+    public static void clickOnCheckBox(ChromeDriver driver,By locator, String checkBoxText){
+        List<WebElement> checkboxes = driver.findElements(locator);
+        
+        for(WebElement checkbox: checkboxes){
+            if(checkbox.getText().trim().equals(checkBoxText)){
+                checkbox.findElement(By.xpath("./../../preceding-sibling::div")).click();
+                break;
+            }
+        }
+
+
+    }
+
+
+    public static void selectFromDropdown(ChromeDriver driver, By locator, String textToSelect) throws Exception {
+       List<WebElement> dropdownElements = driver.findElements(locator);
+       
+       for(WebElement dropdownElement: dropdownElements){
+           if(dropdownElement.getText().trim().equals(textToSelect)){
+            dropdownElement.click();
+            break;
+            }
+        }
+    }
+
+
+
+    public static void handleAlert(ChromeDriver driver, Boolean confirm) throws InterruptedException{
+        Alert alert = driver.switchTo().alert();
+        Thread.sleep(3000);
+
+        if(confirm){
+            alert.accept();
+
+        }else{
+            alert.dismiss();
+        }
+    }
+
+    public static void click(ChromeDriver driver, By locator) throws InterruptedException{
+
+     driver.findElement(locator).click();
+     Thread.sleep(3000);
+    }
+
+
+    public static String returnText(ChromeDriver driver, By locator){
+        
+        return driver.findElement(locator).getText();
+    }
+    
 }
+
